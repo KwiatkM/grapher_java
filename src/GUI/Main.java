893ba96d1b23d5dc396696f855937d5acf0a339d
Grzegorz Porycki <poryckig@gmail.com>
2022-06-10 22:41:12 +0200

    inicjalizacja galezi

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,12 +1,21 @@
 package GUI;
 
 import grapher.Graf;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Aplikacja");
+        primaryStage.show();
+    }
 
-    public static void main( String[] args){
+    public static void main(String[] args){
         System.out.println("grapher_java");
         Graf graf = new Graf(5,5,1,0,10);
         graf.WypiszGraf();
+
+        launch(args);
     }
 }
