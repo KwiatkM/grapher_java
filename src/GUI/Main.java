43800c99f75bea08f01f7c64dc150752f26af48b
Michał Kwiatkowski <01169586@pw.edu.pl>
2022-06-10 21:31:43 +0200

    generowanie nowego grafu

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,8 +1,12 @@
 package GUI;
 
+import grapher.Graf;
+
 public class Main {
 
     public static void main( String[] args){
         System.out.println("grapher_java");
+        Graf graf = new Graf(5,5,1,0,10);
+        graf.WypiszGraf();
     }
 }
