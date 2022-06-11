af932494aaf0b039c3efde4bb2fa4151d8b056d0
Michał Kwiatkowski <01169586@pw.edu.pl>
2022-06-11 14:01:00 +0200

    Wczytywanie grafu z pliku

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
     public static void main( String[] args) throws IOException {
         System.out.println("grapher_java");
-        Graf graf = new Graf(5,5,1,0,10);
-        graf.generujKrawedzie();
+        Graf graf = new Graf(5,6,1,0,10);
+        //Graf graf1 = new Graf("PlikTestowy");
         //graf.wypiszGraf();
         //graf.zapiszDoPliku("PlikTestowy");
+        graf.wypiszGraf();
     }
 }
--- src/grapher/Graf.java
+++ src/grapher/Graf.java
@@ -1,12 +1,12 @@
 package grapher;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Graf  {
     private Wierzcholek[] wierzcholki;
@@ -29,33 +29,90 @@ public class Graf  {
         return nrWierzcholka % wymiarY != 0;
     }
 
-    public Graf(int X, int Y, double szansaNaKrawedz, double wagaOd, double wagaDo){
-        wymiarX = X;
-        wymiarY = Y;
-        this.szansaNaKrawedz = szansaNaKrawedz;
-        this.wagaOd = wagaOd;
-        this.wagaDo = wagaDo;
+    private int nrIndeksuGora(int nrWierzcholka){
+        return nrWierzcholka - wymiarY;
+    }
+    private int nrIndeksuPrawo(int nrWierzcholka){
+        return nrWierzcholka +1;
+    }
+    private int nrIndeksuDol(int nrWierzcholka){
+        return nrWierzcholka + wymiarY;
+    }
+    private int nrIndeksuLewo(int nrWierzcholka){
+        return nrWierzcholka - 1;
+    }
 
+    private void inicjalizajaGrafu(){
         wierzcholki = new Wierzcholek[wymiarX * wymiarY];
         for(int i = 0; i < wymiarY * wymiarX; i++){
             wierzcholki[i] = new Wierzcholek();
         }
     }
 
-    public void generujKrawedzie(){
+    public Graf(int X, int Y, double szansaNaKrawedz, double wagaOd, double wagaDo){
+        wymiarX = X;
+        wymiarY = Y;
+        this.szansaNaKrawedz = szansaNaKrawedz;
+        this.wagaOd = wagaOd;
+        this.wagaDo = wagaDo;
+        inicjalizajaGrafu();
+        generujKrawedzie();
+    }
+
+
+    public Graf (String sciezkaDoPliku) throws FileNotFoundException {
+        File plik = new File(sciezkaDoPliku);
+        Scanner scanner = new Scanner(plik);
+        this.wymiarX = scanner.nextInt();
+        this.wymiarY = scanner.nextInt();
+        scanner.nextLine();
+
+        inicjalizajaGrafu();
+
+        String regex = "([0-9]+) :([0-9]+.[0-9]+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+
+        String tmp;
+        for ( int i = 0; i < wymiarY*wymiarX; i++){
+            System.out.println("Wczytuje wierzcholek nr " + (i+1));
+            tmp = scanner.nextLine();
+            matcher =pattern.matcher(tmp);
+            while(matcher.find()){
+                /*if(Integer.parseInt(matcher.group(1)) == nrIndeksuGora(i)){
+                    wierzcholki[nrIndeksuGora(i)].setKrawedz_dol(Double.parseDouble(matcher.group(2)));
+                    continue;
+                }*/
+                if(Integer.parseInt(matcher.group(1)) == nrIndeksuPrawo(i)){
+                    wierzcholki[i].setKrawedz_prawo(Double.parseDouble(matcher.group(2)));
+                    continue;
+                }
+                if(Integer.parseInt(matcher.group(1)) == nrIndeksuDol(i)){
+                    wierzcholki[i].setKrawedz_dol(Double.parseDouble(matcher.group(2)));
+                    continue;
+                }
+                /*if(Integer.parseInt(matcher.group(1)) == nrIndeksuLewo(i)){
+                    wierzcholki[nrIndeksuLewo(i)].setKrawedz_prawo(Double.parseDouble(matcher.group(2)));
+
+                }*/
+            }
+        }
+    }
+
+    private void generujKrawedzie(){
         for(int i = 0; i < wymiarY * wymiarX; i++){
             if((Math.random() < szansaNaKrawedz) && jestKrawedzPrawo(i)){
-                wierzcholki[i].setKrawedz_prawo(Math.random() * (wagaDo - wagaOd) + wagaDo);
+                wierzcholki[i].setKrawedz_prawo(Math.random() * (wagaDo - wagaOd) + wagaOd);
             }
             if((Math.random() < szansaNaKrawedz) && jestKrawedzDol(i)){
-                wierzcholki[i].setKrawedz_dol(Math.random() * (wagaDo - wagaOd) + wagaDo);
+                wierzcholki[i].setKrawedz_dol(Math.random() * (wagaDo - wagaOd) + wagaOd);
             }
         }
     }
 
     public void zapiszDoPliku(String sciezkaDoPliku) throws IOException {
         File plik = new File(sciezkaDoPliku);
-        System.out.println(plik.getAbsolutePath());
+        //System.out.println(plik.getAbsolutePath());
         FileWriter fw = new FileWriter(plik);
         fw.write(wymiarX + " " + wymiarY + "\n");
         for(int i = 0; i < wymiarY * wymiarX; i++){
